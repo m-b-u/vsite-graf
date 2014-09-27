e9e25a707851df0a0eac49ad3c7395ea00262bdd
Mirko Bulaja <mirko.bulaja@avl.com>
2014-09-27 08:53:25 +0200

    Added linear view, but figures are still messed up

--- 1_uvod/10_raster.py
+++ 1_uvod/10_raster.py
@@ -4,22 +4,46 @@ from matplotlib.colors import LinearSegmentedColormap
 import matplotlib.cm as cm
 import numpy as np
 
+def demo_imagerepresentation():
+	im = np.zeros ((16,16),dtype=np.uint8)
+	im[2,2] = 255
+
+	fig = plt.figure(1)
+	plot = fig.add_subplot(1,1,1)	
+	ax = plot.imshow(im, cm.gray, interpolation = 'none')
+
+	plt.show()
+
+	# represent same data as 1D array
+	im_linear = im.ravel()
+	# turn the first 'pixel' on
+	im_linear[0] = 255
+	im_linear[-1] = 64
+
+	#plot = fig.add_subplot (2,1,1)
+	# just for the need of imshow
+	#im_ = np.reshape(im, (1,np.product(im.shape)))
+	#ax = plot.imshow(im_, cm.gray, interpolation = 'none')
+
+	#plt.tight_layout()
+
+	#plt.draw()
+	#praw_input()
+	# ax.set_data(im)
+	#fig.canvas.draw() # Tk error?
+	#plt.show()
+	# second plot not shown?
+	plt.show()
 
 if __name__=='__main__':
 
-	im = np.zeros ((16,16),dtype=np.uint8)
-	im[2,2] = 200
-	fig = plt.figure(1)
+	demo_imagerepresentation()
 
-	plot = fig.add_subplot(1,1,1)	
-
-	plot.imshow(im, cm.gray, interpolation = 'none')
-
-	plt.show()
 	print "Part 2"
 
 	xres = 20
 	yres = 20
+
 
 #	im_r = np.zeros ((64,64),dtype=np.uint8)
 #	im_g = np.zeros ((64,64),dtype=np.uint8)
@@ -38,6 +62,12 @@ if __name__=='__main__':
 	im_rgb[...,0] = im_r
 	im_rgb[...,1] = im_g
 	im_rgb[...,2] = im_b
+
+	# alternative:
+	#  im_rgb = np.zeros((xres, yres, 3), dtype=np.uint8)
+	#  im=im_rgb.view(dtype=[('r', 'u1'), ('g', 'u1'), ('b', 'u1')]) [:,:,0]
+	#  im['r'] = .......
+
 
 	fig = plt.figure(2, (8., 8.)) 
 	grid = ImageGrid(fig, 222, # similar to subplot(111)
