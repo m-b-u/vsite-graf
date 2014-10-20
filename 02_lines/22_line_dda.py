c25f4a343c5b8e434773b038bef8336eaa4839de
Mirko Bulaja <mirko.bulaja@vsite.hr>
2014-10-21 00:45:44 +0200

    Line DDA error plot layout improvement

--- 02_lines/22_line_dda.py
+++ 02_lines/22_line_dda.py
@@ -3,6 +3,7 @@ from __future__ import division
 import numpy as np
 
 import matplotlib.pyplot as plt
+from matplotlib import gridspec
 import matplotlib.cm as cm
 
 from utils import get_grayscale_image, putpixel, plt_show
@@ -24,7 +25,9 @@ def show_line_dda_with_rounding_error(xres, yres):
         args['sharey'] = True
 
     error = line_dda(im, *(p1 + p2 + ( 255, )) )
-    fig, axis = plt.subplots( 2, **args )
+    #fig, axis = plt.subplots( 2, **args )
+    grid = gridspec.GridSpec (2, 1, height_ratios = [2, 1])
+    axis = [plt.subplot(grid[i]) for i in [0, 1]]
     
     axis[0].imshow(im, cm.gray, interpolation = 'nearest')
     axis[0].plot( *zip(p1, p2), color='g' )
@@ -35,6 +38,8 @@ def show_line_dda_with_rounding_error(xres, yres):
     #axis[1].set_aspect(1)
     #axis[1].set_autoscaley_on(False)
     #axis[1].set_ylim( -0.6, 0.6 )
+
+    plt.tight_layout()
 
     plt_show()
 
