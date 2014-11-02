a1ba5743d408b44e958ac3c142dac4730c0b3321
Mirko Bulaja <mirko.bulaja@vsite.hr>
2014-11-02 14:44:24 +0100

    Initial Imgproc commit Lots of stuff does not work. Buttons w/o labels, etc.

---
+++ 04_bitmap/47_improc.py
@@ -0,0 +1,141 @@
+""" Simple GUI for demo of imgproc module """
+
+
+import imgproc
+import sys
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+from matplotlib.widgets import Button
+
+filename = "../samples/cart_1a34309r.jpg" if len(sys.argv)<2 else sys.argv[1]
+
+class ImageHandlers(object):
+    """ Action methods for ImageDialog """
+
+    def __init__(self):
+        super(ImageHandlers, self).__init__()
+        self.image = None
+        self.preview = None
+
+    def do_levels_test(self, event):
+        """ Test level adjustments with some predefined settings."""
+        self.preview = imgproc.adjust_levels(self.image,
+                                             (0.12, 0.8), (0., 1.), 1.0)
+        self.update_display()
+
+    def do_autolevels(self, event):
+        """ Perform auto levels stretch"""
+        self.preview = imgproc.auto_levels(self.image)
+        self.update_display()
+
+    def do_blur(self, event):
+        """ Perform box blur"""
+        self.preview = imgproc.blur_uniform(self.image)
+        self.update_display()
+
+    def do_blur_gaussian(self, event):
+        """ Perform 3x3 Gaussian blur"""
+        self.preview = imgproc.blur_gaussian(self.image)
+        self.update_display()
+
+    def do_sharpen(self, event):
+        """ Do simple cross sharpen"""
+        self.preview = imgproc.sharpen(self.image)
+        self.update_display()
+
+    def apply(self, event):
+        """ Apply the current preview image as the new base image"""
+        self.image = self.preview.copy()
+        self.update_display()
+
+    def reset(self, event):
+        """ Reset the preview image to original"""
+        self.preview = self.image.copy()
+        self.update_display()
+
+    def save(self, event):
+        """Save the modified image"""
+        pass
+
+    def quit(self, event):
+        """ Quit the application."""
+        pass
+
+class ImageDialog(ImageHandlers):
+    """ Control dialog for image editing actions"""
+    def __init__(self):
+        """ Initialize dialog and needed data """
+        super(ImageDialog, self).__init__()
+        self.buttons = []
+        self.axes = []
+        self.fig = None
+        self.image_view = []
+        self.filename = None
+
+    def load_file(self, filename):
+        """ Load specified file into image buffers"""
+        self.filename = filename
+        self.image = imgproc.convert_from_uint8(mpimg.imread(filename))
+        self.preview = self.image.copy()
+
+    def setup_display(self):
+        """ Set up figure for image buffers"""
+        self.fig, axis = plt.subplots(2)
+        self.image_view = [a.imshow(self.image, aspect=1) for a in axis]
+        axis[0].set_title('Image')
+        axis[1].set_title('Preview')
+        return self.fig, axis, self.image_view
+
+    def setup_controls(self):
+        """ Set up button axes"""
+        buttons = [('Auto levels', self.do_autolevels),
+                   ('Levels1', self.do_levels_test),
+                   ('Blur uniform', self.do_blur),
+                   ('Blur gaussian', self.do_blur_gaussian),
+                   ('Sharpen', self.do_sharpen),
+                   ('Apply', self.apply),
+                   ('Reset', self.reset),
+                   ('Save', self.save),
+                   ('Quit', self.quit)]
+        padding = 0.01
+        start, end = 0.1, 0.8
+        hspace = (end - start) / len(buttons)
+        for i, b in enumerate(buttons):
+            axis = self.fig.add_axes([start + i*hspace, 0.03,
+                                      start + (i+1)*hspace - padding, 0.03])
+            self.axes.append(axis)
+            button = Button(axis, b[0])
+            button.on_clicked(b[1])
+            self.buttons.append(button)
+
+    def run(self):
+        """ Start the dialog """
+        plt.show()
+
+    def update_display(self):
+        """ Update images after some operation """
+        self.image_view[0].set_data(self.image)
+        self.image_view[1].set_data(self.preview)
+        self.fig.canvas.draw_idle()
+
+dlg = ImageDialog()
+dlg.load_file(filename)
+dlg.setup_display()
+dlg.setup_controls()
+
+
+action_map = {'autolevels': dlg.do_autolevels,
+              'levels_test': dlg.do_levels_test,
+              'blur': dlg.do_blur,
+              'sharpen':  dlg.do_sharpen}
+
+if len(sys.argv)>2:
+    fname = sys.argv[2]
+    function = action_map.get(fname, None)
+    if function:
+        print "Perform: ", fname
+        function(None)
+
+dlg.run()
---
+++ 04_bitmap/imgproc.py
@@ -0,0 +1,144 @@
+""" Module with basic image processing functions"""
+
+import numpy as np
+from utils import is_rgb_image, get_component_view
+
+def convert_from_uint8(img):
+    """Convert image in uint8 [0..255] to [0.0..1.0] float32"""
+    return img.astype(np.float32) / 255.0
+
+def convert_to_uint8(img):
+    """Convert image in [0.0..1.0] float32 to uint8 [0..255]"""
+    return (img * 255).astype(np.uint8)
+
+def get_image_minmax (img):
+    """Get minimum and maximum value of any pixel component in the image"""
+    print "Image min, max: ", img.min(), img.max()
+    return img.min(), img.max()
+
+def adjust_levels(img, in_levels, out_levels, gamma=1.0):
+    """ Perform level adjustments and gamma correction
+    Args: img - image to be adjusted
+          in_levels - (input_low, input_high) tuple,
+                      image will be taken as if these
+                      were the input limits, regardless of real values
+          out_levels - (output_low, output_high),
+                       these will be limits for output image
+          gamma - taken to be the gamma of the input image
+    Returns: adjusted image (copy)
+    """
+    buf = img.copy()
+    dtype = img.dtype
+    if dtype == np.uint8:
+        in_levels = (in_levels[0]/255.0, in_levels[1]/255.0)
+        out_levels = (out_levels[0]/255.0, out_levels[1]/255.0)
+        buf = convert_from_uint8(img)
+    get_image_minmax(buf)
+    buf = np.power ((buf - in_levels[0]) / (in_levels[1] - in_levels[0]),
+                    1/gamma)
+    # if gamma is not 1 input image should be converted to [0-1] float values
+    np.clip(buf, 0., 1., out=buf)
+    get_image_minmax(buf)
+    buf *= (out_levels[1]-out_levels[0])
+    get_image_minmax(buf)
+    buf += out_levels[0]
+    get_image_minmax(buf)
+
+    np.clip(buf, 0., 1., out=buf)
+    get_image_minmax(buf)
+
+    if dtype == np.uint8:
+        return convert_to_uint8(buf)
+    else:
+        return buf
+
+def auto_levels(img):
+    """ Determine the absolute minimum and maximum of the input image 
+    and call adjust_levels to stretch it to [0..1] or [0..255] 
+    depending on type of input image"""
+
+    out_levels = (0, 255) if img.dtype == np.uint8 else (0.0, 1.0)
+    return adjust_levels(img, get_image_minmax(img), out_levels)
+
+def apply_with_amount(img, filter_func, amount):
+    """ Call the 'filter_func' without arguments on the image and return 
+    linear combination of filtered and original image"""
+    return img * (1. - amount) + filter_func(img) * amount
+
+#def apply_kernel(image, kernel):
+ #   img = image.copy()
+ #   if is_rgb_image(img):
+ #       im = get_component_view(img)
+ #       im['r'] = np.convolve(im['r'], kernel)
+ #       im['g'] = np.convolve(im['g'], kernel)
+ #       im['b'] = np.convolve(im['b'], kernel)
+ #       return im
+ #   else:
+ #       return np.convolve(image, kernel)
+
+
+import scipy.signal
+#import scipy.ndimage
+# Faster alternative would be: Horizontal and vertical pass with 1D convolution
+# How to split kernel automaticaly?
+
+def normalize_kernel(kernel):
+    """ Normalize the convolution kernel so that sum of all
+    element equals 1"""
+    sum_ = np.sum(kernel)
+    if abs(sum_) > 1e-10:
+        return kernel / sum_
+
+def apply_kernel(image, kernel):
+    """Apply the 2D convolution kernel to image"""
+    img = image.copy()
+
+    def conv(mat):
+        """ Apply the kernel to 2D matrix"""
+        return scipy.signal.convolve2d(mat, kernel)
+
+    if is_rgb_image(img):
+        im = get_component_view(img)
+        return np.dstack([conv(im['r']), conv(im['g']), conv(im['b'])])
+    else:
+        return conv(img)
+
+def blur_uniform(img):
+    """ Kernel op: 3x3 box blur"""
+    kernel = np.ones((3, 3))
+    kernel /= float(kernel.size)
+    return apply_kernel(img, kernel)
+
+def blur_gaussian(img):
+    """ Kernel op: 3x3 Gaussian blur"""
+    kernel = np.array([[1., 2., 1.],
+                      [2., 4., 2.],
+                      [1., 2., 1.]], dtype=np.float32)
+    kernel = normalize_kernel(kernel)
+    return apply_kernel(img, kernel)
+
+def sharpen(img):
+    """ Kernel op: 3x3 Cross sharpen"""
+    kernel = np.array([[0., 1., 0.], 
+                      [1., -5., 1.],
+                      [0., 1., 0.]], dtype=np.float32)
+    kernel = normalize_kernel(kernel) 
+    return apply_kernel(img, kernel)
+
+def unsharp(img):
+    """ Kernel op: 5x5 unsharp (without mask)"""
+    kernel = np.array([[1., 4., 6., 4., 1.],
+                      [4., 16., 24., 16., 4.],
+                      [6., 24., -476., 24., 6],
+                      [4., 16., 24., 16., 4.],
+                      [1., 4., 6., 4., 1.]], dtype=np.float32)
+    kernel = normalize_kernel(kernel) 
+    return apply_kernel(img, kernel)
+
+def edge_hv(img):
+    """ Horizontal and vertical edge detection """
+    kernel = np.array([[-1., -1., -1.],
+                      [-1., 8., -1.],
+                      [-1., -1., -1.]], dtype=np.float32)
+    # this kernel is not normalized: sum is zero
+    return apply_kernel(img, kernel)
