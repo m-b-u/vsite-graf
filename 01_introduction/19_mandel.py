6532593cb7566c3ce03ee458037fab742a9df135
Mirko Bulaja <mirko.bulaja@vsite.hr>
2015-10-21 07:07:09 +0200

    Added click handler for fixed step zoom.

--- 01_introduction/19_mandel.py
+++ 01_introduction/19_mandel.py
@@ -5,12 +5,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
-
-
-
-""" Iteration loop for each point, returns value, iteration number
-and whether point is in Mandelbrot set"""
 def mandel_point(c, max_iter):
+    """ Iteration loop for each point, returns value, iteration number
+and whether point is in Mandelbrot set"""
     z = 0
     for i in xrange(max_iter):
         z = z*z + c
@@ -18,29 +15,53 @@ def mandel_point(c, max_iter):
             return z, i, False
     return z, i, True
 
-""" Simplest approach, iterating over every point """
-def render_mandel_iterative(img, ll_corner, ur_corner, max_iter = 100):
+
+def render_mandel_iterative(img, ll_corner, ur_corner, max_iter=100):
+    """ Simplest approach, iterating over every point """
     size = get_image_size(img)
     x_axis = np.linspace(ll_corner.real, ur_corner.real, num=size[0])
     y_axis = np.linspace(ur_corner.imag, ll_corner.imag, num=size[1])
     for i in xrange(size[1]):
         for j in xrange(size[0]):
-            val, iter, max_reached = mandel_point(complex(x_axis[j],y_axis[i]), max_iter)
+            val, iteration, max_reached = mandel_point(complex(x_axis[j], y_axis[i]), max_iter)
             if not max_reached:
-                putpixel(img, j, i, iter)
+                putpixel(img, j, i, iteration)
+            else:
+                putpixel(img, j, i, 0)
 
-        print 
 
-if __name__=='__main__':
-  xres, yres = (300, 200)
-  img = get_grayscale_image(xres, yres)
+default_box = [complex(-2.5, -1.5), complex(1.5, 1.5)]
+box = default_box[:]
 
-  ll_corner = complex(-2.5, -1.5)
-  ur_corner = complex(1.5, 1.5)
-  max_iter = 60
+# take a look here to show image pixel per pixel
+# https://stackoverflow.com/questions/8056458/display-image-with-a-zoom-1-with-matplotlib-imshow-how-to
+if __name__ == '__main__':
+    xres, yres = (400, 300)
+    max_iter = 256
+    img = get_grayscale_image(xres, yres)
+
+    fig, ax = plt.subplots(1)
   
-  render_mandel_iterative(img, ll_corner, ur_corner, max_iter)
+    def on_click(event):
+        """Plot click event handler - zoom into set"""
+        global box
+        print event.inaxes
+        box_width = (box[1].real - box[0].real)
+        box_height = (box[1].imag - box[0].imag)
+        center = complex(box[0].real + (float(event.xdata)/get_image_size(img)[0])*box_width,
+                         box[0].imag + (1.0-(float(event.ydata)/get_image_size(img)[1]))*box_height)
+        new_box_size = complex(box_width/1.5, box_height/1.5)
+        box[0] = center - new_box_size/2
+        box[1] = center + new_box_size/2
 
-  fig, ax = plt.subplots(1)
-  ax.imshow(img, cm.spectral, interpolation='nearest')
-  plt_show()
+        render_mandel_iterative(img, box[0], box[1], max_iter)
+        ax.imshow(img, cm.spectral, interpolation='nearest')
+        plt_show()
+
+
+    cid = fig.canvas.mpl_connect('button_press_event', on_click)
+
+    render_mandel_iterative(img, box[0], box[1], max_iter)
+    ax.imshow(img, cm.spectral, interpolation='nearest')
+    plt_show()
+    fig.canvas.mpl_disconnect(cid)
