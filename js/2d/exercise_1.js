70352a305e3710e03e179794e9e7a28b18608145
Mirko Bulaja <mirko.bulaja@vsite.hr>
2016-10-10 07:50:58 +0000

    Revert default fill

--- js/2d/exercise_1.js
+++ js/2d/exercise_1.js
@@ -5,7 +5,7 @@ function startExercise1() {
     var div = document.getElementById("main");
     
     var fb = createFramebuffer(60,15);
-    clearFramebuffer(fb); 
+    clearFramebuffer(fb, ".".charCodeAt(0)); 
     putPixel(fb, 3, 5, "*".charCodeAt(0));
 
     consoleprintFramebuffer(fb);
