e5ada1746011d2fa883c60354029fed2e7c7117c
Mirko Bulaja <mirko.bulaja@vsite.hr>
2016-10-11 19:59:02 +0200

    Temp commit, does not work

--- 01_introduction/raster_canvas.js
+++ 01_introduction/raster_canvas.js
@@ -4,22 +4,83 @@ function getFrameBufferFromCanvas(id) {
     return ctx.getImageData(0, 0, canvas.width, canvas.height);
 }
 
+function getFrameBufferFromCanvas_ex(canvas) {
+    var ctx = canvas.getContext("2d");
+    return ctx.getImageData(0, 0, canvas.width, canvas.height);
+}
+
+
 function putFrameBufferToCanvas(id, data) {
     var canvas = document.getElementById(id);
     var ctx = canvas.getContext("2d");
     ctx.putImageData(data, 0, 0);
 }
 
-function startExercise1b_canvas() {
-    var imgData = getFrameBufferFromCanvas("fb");
-    for (var i=1000; i<1200; i+=4) {
-	imgData.data[i]=100;
-	imgData.data[i+1]=150;
-	imgData.data[i+2]=200;
-	imgData.data[i+3]=255;
-    }
-    
-    putFrameBufferToCanvas("fb", imgData);
-    putFrameBufferToCanvas("fb2", imgData);
+function putCanvasToCanvas_ex(canvas, canvasData) {
+    var ctx = canvas.getContext("2d");
+    // ctx.putImageData(data, 0, 0, 0, 0, canvas.width, canvas.height);
+    var ctx2 = canvasData.getContext("2d");
+    ctx.drawImage(ctx2, 0, 0, canvas.width, canvas.height);
 }
 
+
+function clearFrameBuffer(fb) {
+    for (var i=0; i<fb.data.length; ) {
+	fb.data[i++]=0;
+	fb.data[i++]=0;
+	fb.data[i++]=0;
+	fb.data[i++]=255;
+    }
+}
+
+function putPixel(fb, x, y, value)
+{
+    i = (fb.width * y + x) * 4;
+    fb.data[i++] = value[0];
+    fb.data[i++] = value[1];
+    fb.data[i++] = value[2];
+    fb.data[i++] = value[3];
+}
+
+function gray(val) {
+    return [val, val, val, 255];
+}
+
+function getRandomInt(val) {
+    return Math.floor(Math.random()*val);
+}
+
+function createImageData(width, height) {
+    var canvas = document.createElement('canvas');
+    canvas.width = width;
+    canvas.height = height;
+    return canvas;
+}
+
+function startExercise1b_canvas() {
+    var imgData;
+    
+    var canvas = document.getElementById("fb1");
+    var canvasSmall = createImageData(canvas.width/20, canvas.height/20);
+    imgData = getFrameBufferFromCanvas_ex(canvasSmall);
+    clearFrameBuffer(imgData);
+    putPixel(imgData, 5, 3, [255, 0, 0, 255]);
+    putPixel(imgData, 8, 7, [0, 255, 0, 255]);
+    var contextSmall = canvasSmall.getContext("2d");
+    contextSmall.putImageData(imgData, 0, 0);
+
+    putFrameBufferToCanvas_ex(canvas, canvasSmall);
+
+    imgData = getFrameBufferFromCanvas("fb3");
+    clearFrameBuffer(imgData);
+    for (var i=0; i<100; i++) {
+	putPixel(imgData, getRandomInt(imgData.width), getRandomInt(imgData.height), gray(getRandomInt(255)));
+    }
+    
+    putFrameBufferToCanvas("fb3", imgData);
+}
+
+
+
+
+// https://stackoverflow.com/questions/19129644/how-to-pixelate-an-image-with-canvas-and-javascript
